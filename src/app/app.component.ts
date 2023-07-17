4c41570a0d5f971d7dd84011c49801a127bcf5bc
Tony <tony@DESKTOP-C5TKN10.localdomain>
2023-07-17 20:32:18 +1000

    Added bootstrap and also array of objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Pages';
+  images = [
+    {
+      title: "At the Beach",
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: "At the Woods",
+      url: 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+    },
+    {
+      title: "At the Town",
+      url: 'https://images.unsplash.com/photo-1609971757431-439cf7b4141b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YXQlMjB0aGUlMjB0b3dufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    }
+  ];
 }
